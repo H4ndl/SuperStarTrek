c2bfcd3115c20df852b19c38d997cef6935bb089
Kyle <ksummerf@uwyo.edu>
2018-03-15 10:23:34 -0600

    Initial commit

---
+++ Shield.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperStarTrek
+{
+    public class Shield
+    {
+        bool isUp = false;
+        int power = 8000;
+        public int maxPower = 10000;
+
+        public bool IsUp()
+        {
+            return isUp;
+        }
+
+        public void RaiseShield()
+        {
+            isUp = true;
+        }
+
+        public int GetCurrrentEnergy()
+        {
+            return power;
+        }
+
+        public void TransferEnergy(int amountToTransfer)
+        {
+            power += amountToTransfer;
+            if (power > maxPower)
+            {
+                power = maxPower;
+            }
+            if (power < 0)
+            {
+                power = 0;
+            }
+        }
+    }
+}
